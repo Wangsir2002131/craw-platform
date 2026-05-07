81a700f21e08f5f20085aeb65aecb287c73f9e1b
“WangYiLei” <1969233231@qq.com>
2026-05-07 13:54:03 +0800

    更新

--- shared-methods/shared_methods.py
+++ shared-methods/shared_methods.py
@@ -65,8 +65,8 @@ DB_CONFIG = {
     'host': '127.0.0.1',
     'port': 3306,
     'user': 'root',
-    'password': 'root',
-    'db': 'geo',
+    'password': '123456',
+    'db': 'test',
     'charset': 'utf8mb4',
     'cursorclass': pymysql.cursors.DictCursor
 }
